28d24744dc384b3e7d1787a1b2dbf1a22ba95596
sadasiannerd <106918826+sadasiannerd@users.noreply.github.com>
2024-12-01 12:18:21 -0500

    initial commit

---
+++ backend/routes.py
@@ -0,0 +1,101 @@
+from . import app
+import os
+import json
+import pymongo
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
+from pymongo import MongoClient
+from bson import json_util
+from pymongo.errors import OperationFailure
+from pymongo.results import InsertOneResult
+from bson.objectid import ObjectId
+import sys
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "data", "songs.json")
+songs_list: list = json.load(open(json_url))
+
+# client = MongoClient(
+#     f"mongodb://{app.config['MONGO_USERNAME']}:{app.config['MONGO_PASSWORD']}@localhost")
+mongodb_service = os.environ.get('MONGODB_SERVICE')
+mongodb_username = os.environ.get('MONGODB_USERNAME')
+mongodb_password = os.environ.get('MONGODB_PASSWORD')
+mongodb_port = os.environ.get('MONGODB_PORT')
+
+print(f'The value of MONGODB_SERVICE is: {mongodb_service}')
+
+if mongodb_service == None:
+    app.logger.error('Missing MongoDB server in the MONGODB_SERVICE variable')
+    # abort(500, 'Missing MongoDB server in the MONGODB_SERVICE variable')
+    sys.exit(1)
+
+if mongodb_username and mongodb_password:
+    url = f"mongodb://{mongodb_username}:{mongodb_password}@{mongodb_service}"
+else:
+    url = f"mongodb://{mongodb_service}"
+
+
+print(f"connecting to url: {url}")
+
+try:
+    client = MongoClient(url)
+except OperationFailure as e:
+    app.logger.error(f"Authentication error: {str(e)}")
+
+db = client.songs
+db.songs.drop()
+db.songs.insert_many(songs_list)
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+######################################################################
+# INSERT CODE HERE
+@app.route("/health", methods=["GET"])
+def heatlh():
+    return jsonify({"status": "OK"}), 200
+
+@app.route("/count", methods=["GET"])
+def count():
+    return jsonify({"count": db.songs.count()}), 200
+
+@app.route("/songs", methods=["GET"])
+def get_songs():
+    songs = db.songs.find()
+    #send back as a list of songs
+    return {"songs": parse_json(songs)}, 200
+
+@app.route("/songs/<int:id>", methods=["GET"])
+def get_song(id):
+    song = db.songs.find_one({"_id": ObjectId(id)})
+    if song:
+        return {"song": parse_json(song)}, 200
+    else:
+        return {"message": "song with id not found"}, 404 
+
+@app.route("/song/<int:id>", methods=["POST"])
+def create_song(id):
+    song = request.get_json()
+    if db.songs.find_one({"_id": ObjectId(id)}):
+        return {"Message": f"song with id {id} already present"}, 302
+    song["_id"] = ObjectId(id)
+    db.songs.insert_one(song)
+    return {"Message": "song created successfully"}, 201
+
+@app.route("/song/<int:id>", methods=["PUT"])
+def update_song(id):
+    song = request.get_json()
+    if db.songs.find_one({"_id": ObjectId(id)}):
+        db.songs.update_one({"_id": ObjectId(id)}, {"$set": song})
+        return {"Message": "song updated successfully"}, 200
+    else:
+        return {"Message": "song not found"}, 404
+    
+@app.route("/song/<int:id>", methods=["DELETE"])
+def delete_song(id):
+    if db.songs.find_one({"_id": ObjectId(id)}):
+        db.songs.delete_one({"_id": ObjectId(id)})
+        return {"Message": "song deleted successfully"}, 200
+    else:
+        return {"Message": "song not found"}, 404
+
+######################################################################
